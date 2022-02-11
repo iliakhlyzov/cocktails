be38cb3dd5f9a0bef752f1be3221146772ec93b3
Elena <elena@thecarrot.space>
2022-02-11 17:18:43 +0200

    Add the task description

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# cocktails
-Created with CodeSandbox
+# Setup guide:
+
+1. Clone the repo
+1. Run `npm i`
+1. Run `npm start`
+1. Check `http://localhost:8080/`
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
-import http from "http";
+import http from 'http';
+
+interface Cocktail {
+  id: string; // idDrink
+  name: string; // strDrink
+  category: string; // strCategory
+  ingredients: {
+    name: string; // strIngredient(1-15)
+    measure: string | null; // strMeasure(1-15)
+  }[]
+}
+
+interface CocktailTag {
+  name: string;
+  cocktails: Cocktail[];
+}
 
 /**
  * Tasks
- * 1. Fetch
+ * 1. Fetch cocktails list from https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+ * 2. Map cocktails into the structure of Cocktail interface (see above)
+ * 3. Some cocktails have tags, f.i. "strTags": "IBA,ContemporaryClassic".
+ *   Group cocktails by tags.
+ *   The response should be of type CocktailTag[].
+ *   Your server should respond with this array of cocktail tags
  */
 
 http
   .createServer(function (req, res) {
-    res.write(JSON.stringify(req));
+    res.write('ok');
     res.end();
   })
   .listen(8080);
