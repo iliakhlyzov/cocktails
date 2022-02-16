7dba6c8b0b19fc843918ceb9788ce6f7f1654b10
Ilia Khlyzov <khlyzoff@gmail.com>
2022-02-16 12:18:38 +0300

    fix(npm): types/node-fetch was changed from 3 to 2.6

--- src/services/HttpFetch/HttpFetch.ts
+++ src/services/HttpFetch/HttpFetch.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export class HttpFetch {
   public static async getDrinks(url: URL) {
-    const response = await fetch(url.href).catch(() => {});
+    const response = await fetch(url.href);
     const { drinks } = await response.json();
     if (!(drinks instanceof Array)) {
       throw new Error("There isn't array of drinks in response json!");
